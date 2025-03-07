52e2a803367f5a489f1912e552abe43aec449396
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-03-07 09:40:27 +0330

    🐛 reservation bug fixed

--- alibaba/aeroplane/models.py
+++ alibaba/aeroplane/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from train.models import Customer
 
-class Reservations(models.Model):
+
+
+class Flights(models.Model):
     user = models.ForeignKey(to = Customer)
     time = models.DateTimeField(auto_now_add = True)
     passengers = models.IntegerField()
@@ -10,5 +12,9 @@ class Reservations(models.Model):
     return_time = models.DateField(default = None)
     destination_city = models.CharField(max_length = 100)
     starting_city = models.CharField(max_length = 100)
+    aeroplane_capacity = models.IntegerField()
+    abroad_flight = models.BooleanField()
+    seat = models.IntegerField()
 
-class 
+class Reservations(models.Model):
+    aeroplane_ticket = models.ForeignKey(to = Flights)
