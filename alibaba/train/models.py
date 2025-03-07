0381cbacd7add0f046beec3bf79893561e8074aa
Arasteh Zamen Ghadirli <41484641+ArastehZz@users.noreply.github.com>
2025-03-07 12:59:05 +0330

    khbcjlhv

--- alibaba/train/models.py
+++ alibaba/train/models.py
@@ -12,7 +12,7 @@ class Customer(models.Model):
 class Tarin(models.Model):
     name = models.CharField(max_length=20)
     entrance_date = models.DateTimeField(auto_now_add=True)
-    capacity = models.IntegerField()
+    capacity = models.PositiveIntegerField()
     
 
 class Reservation():
